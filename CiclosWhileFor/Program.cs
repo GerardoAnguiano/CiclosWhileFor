31f0686b3d73f104f6cbd92e7c91ac47076a863a
gerardo anguiano <star-0102@hotmail.com>
2021-04-16 00:50:31 -0700

    CicloFor

--- CiclosWhileFor/Program.cs
+++ CiclosWhileFor/Program.cs
@@ -18,17 +18,20 @@ namespace CiclosWhileFor
             //parte 3 aumento o decremento
 
             int tabla;
-            int i = 1; //parte 1
+           // int i = 1; //parte 1
             Console.WriteLine("ingrese el numero de la tabla que quiere calcular");
             tabla = int.Parse(Console.ReadLine());
 
             //parte 2
-            while (i <= 10)
+            //  while (i <= 10)
+            // {
+            //    Console.WriteLine("" + tabla * i);
+            //  i++; //parte 3
+            //}
+            for (int i = 1; i <= 10; i++) 
             {
                 Console.WriteLine("" + tabla * i);
-                i++; //parte 3
             }
-
             Console.ReadKey();
         }
     }
